ce12195cae27949e328fa27f502226eca3e7f671
Billett Development <kbillettdev@hotmail.com>
2020-04-22 14:52:52 +0100

    implemented firebase utils, adding users to firabse database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,34 @@ const config = {
     appId: "1:989137170459:web:ef525dd7aa8f19f80dc8be"
   };
 
+  export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot =  await userRef.get();
+
+    if(!snapShot.exits){
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+
+        } catch (error) {
+            console.log('errore creating user', error.message)
+        }
+    }
+
+    return userRef;
+  };
+
+  
 
 firebase.initializeApp(config);
 
